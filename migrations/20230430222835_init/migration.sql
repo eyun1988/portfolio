0064ea2967c849853c6cad8332519a9bdaf9afa6
Eddy Yun <eyun1@mail.sfsu.edu>
2023-05-01 09:14:01 -0700

    remove .env

---
+++ migrations/20230430222835_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Data" (
+    "id" SERIAL NOT NULL,
+    "data" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Data_pkey" PRIMARY KEY ("id")
+);
